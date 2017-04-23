fa0e8e8040515d4c4949a68b18604c15c32c1e47
ReeceHub <reecelong@hotmail.com>
2017-04-23 02:24:31 +0100

    Update comments
    Add more comments to be more explicit in what the ParserBot class does.

--- parserbot.py
+++ parserbot.py
@@ -4,28 +4,62 @@ from urllib import parse
 
 
 class ParserBot(HTMLParser):
+    """Simple HTML parser built to extract links and school information from indschools.co.uk"""
     def __init__(self):
+        """The self.school_links list stores links to school profiles on the website.
+        
+        The self.data list stores all the information we are interested in for one school at a time.
+        """
         super().__init__()
         self.base_url = ''
         self.school_links = []
         self.data = []
-
-        self.getting_school_links = False           # a switch - are we currently trying to pull links?
-        self.getting_school_info = False            # a switch - are we currently trying to pull school data?
-        self.looking_at_label = False               # a switch - is the ParserBot currently looking at a label element?
-        self.waiting_for_phone = False              # a switch - have we found the area where the phone number is?
-        self.getting_data = False                   # a switch - are we allowing data to be stored at this time?
-        self.getting_ages = False                   # a switch - have we found the area where the age ranges are?
-
-        self.counter = 0                            # a counter - helps us know when to turn off getting_data switch
-        self.agecounter = 0                         # a counter - helps us know when to stop pulling age ranges
-        self.waitingcounter = 0                     # a counter - helps us know when to stop expecting phone number
+        
+        # A switch - are we currently trying to pull links?
+        self.getting_school_links = False
+        
+        # A switch - are we currently trying to pull school data?
+        self.getting_school_info = False
+        
+        # A switch - is the parser currently looking at a label element?
+        self.looking_at_label = False
+        
+        # A switch - have we found the area where the phone number is?
+        self.waiting_for_phone = False
+        
+        # A switch - are we allowing data to be stored at this time?
+        self.getting_data = False
+        
+        # A switch - have we found the area where the age ranges are?
+        self.getting_ages = False                   
+        
+        # A counter - helps us know when to turn off getting_data switch.
+        self.counter = 0
+        
+        # A counter - helps us know when to stop pulling age ranges.
+        self.agecounter = 0                         
+        
+        # A counter - helps us know when to stop expecting phone number.
+        self.waitingcounter = 0                     
 
     def refresh(self):
+        """Clear the data we have stored in our 'data' container."""
         self.data = []
 
     def handle_starttag(self, tag, attrs):
-        if self.getting_school_links is True:   # reading 'homepage' list
+        """Override method in HTMLParser.
+        
+        If we are currently searching for school links from an area 'homepage', the self.getting_school_links switch will
+        be True thanks to the get_school_links() method we called.  This handle_starttag() method will tell the parser to add
+        all school links it can see in the area to our self.school_links container.
+        
+        If we are instead searching for school information (i.e. address etc.) from a school's profile on the website, the
+        self.getting_school_info switch will be True thanks to the get_school_info() method we called.  This handle_starttag()
+        method will turn on additional switches when it comes accross information we care about (in preparation for scraping).
+        """
+        
+        # 'self.getting_school_links' is switched 'on' to True when we want read the list of schools from an area's homepage
+        if self.getting_school_links is True:
             if tag == 'div':
                 for (attr_name, value) in attrs:
                     if attr_name == 'onclick':
@@ -33,8 +67,9 @@ class ParserBot(HTMLParser):
                         sub_url = end_part[1][2:-3]
                         full_url = parse.urljoin(self.base_url, sub_url)
                         self.school_links.append(full_url)
-
-        if self.getting_school_info is True:    # reading a school info page
+        
+        # 'self.getting_school_info' is switched 'on' to True when we want read the information held on a school's profile page
+        if self.getting_school_info is True:
             if tag == 'h1':
                 for (attr_name, value) in attrs:
                     if attr_name == 'class' and 'SchoolNameTitle' in value:
@@ -50,6 +85,10 @@ class ParserBot(HTMLParser):
                         self.getting_ages = True
 
     def handle_data(self, data):
+        """Override method in HTMLParser.
+        
+        Tell our parser to add data to our self.data container once all appropriate switches have been turned on.
+        """
         if self.getting_school_info is True:        # only need to handle data on info pages
             if self.getting_data is True:
                 self.data.append(data)
@@ -70,8 +109,17 @@ class ParserBot(HTMLParser):
                     self.agecounter = 0
 
     def handle_endtag(self, tag):
-        if self.getting_school_info is True:        # only need to worry about turning the data switch off...
-            if self.getting_data is True:           # ...if we're trying to pull info from the page
+        """Override method in HTMLParser.
+        
+        This method decides when to turn switches off (i.e. to False).
+        """
+        
+        # We only need to worry about turning switches off when we're pulling data from a school's profile page.
+        if self.getting_school_info is True:
+            
+            # Use a counter to turn off the self.getting_data switch once we've passed two <div> tags because this
+            # marks the end of the address info section.
+            if self.getting_data is True:
                 if tag == 'div':
                     self.counter += 1
                     if self.counter == 2:
@@ -80,16 +128,18 @@ class ParserBot(HTMLParser):
 
             if self.looking_at_label is True:
                 if tag == 'label':
-                    self.looking_at_label = False       # finished looking at a label
+                    self.looking_at_label = False        # We have now passed the label.
 
+            # Use another counter to stop looking for a phone number after we've passed two <div> tags.
             if self.waiting_for_phone is True:
                 if tag == 'div':
                     self.waitingcounter += 1
                     if self.waitingcounter == 2:
-                        self.waiting_for_phone = False      # we've now passed the phone number
+                        self.waiting_for_phone = False        # We have now passed the phone number.
                         self.waitingcounter = 0
 
-    def get_school_links(self, url):    # pull links from area page
+    def get_school_links(self, url):
+        """Pull links from an area page"""
         try:
             response = urlopen(url)
         except:
@@ -107,7 +157,8 @@ class ParserBot(HTMLParser):
             print(url + " doesn't contain permitted HTML content type.  Page was not fed to your ParserBot.")
             return
 
-    def get_school_info(self, url):     # pull school's name, address, phone, ages from its indschools.co.uk page
+    def get_school_info(self, url):
+        """Pull a school's name, address, phone, and ages from its indschools.co.uk page"""
         try:
             response = urlopen(url)
         except:
